0ff01f5ddf69b713fa3afdcd28e0a92474356580
louise ouldhaddi <ouldhaddi@gmail.com>
2022-06-07 18:37:54 +0200

    seeds + associations + squelette home page

--- app/models/coworking.rb
+++ app/models/coworking.rb
@@ -1,3 +1,4 @@
 class Coworking < ApplicationRecord
   has_many :bookings, dependent: :destroy
+  belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+puts "Cleaning database..."
+Coworking.destroy_all
+User.destroy_all
+Booking.destroy_all
+
+puts "Creating users..."
+User.create(email: "louise@louise.com", password: "123456")
+5.times do
+  new_user = User.new(email: Faker::Internet.email, password: "123456")
+  new_user.save!
+end
+
+puts "Creating coworkings..."
+10.times do
+  coworking = Coworking.new(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    description: Faker::ChuckNorris.fact,
+    price_per_hour: (0..10).to_a.sample,
+    user: User.all.sample
+  )
+  coworking.save!
+end
+
+puts "Creating bookings..."
+10.times do
+  booking = Booking.new(
+    start_time: 14,
+    end_time: 18,
+    total_price: 24,
+    user: User.all.sample,
+    coworking: Coworking.all.sample
+  )
+  booking.save!
+end
+
+puts "Finished!"
